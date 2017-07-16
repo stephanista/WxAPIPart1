ea2e3c0840676d149f474635cb4067202d01bd7d
stephanista <houser16@uw.edu>
2017-07-16 15:44:54 -0700

    Creates basic files, API url, and empty functions

---
+++ weather.js
@@ -0,0 +1,11 @@
+const apiURL = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather";
+const appID = "a4192ac11c66b463d93c577e64fda186"; //yes thats mine
+let debug = null
+
+function getSeaWx() {
+
+}
+
+function getLondonWx() {
+
+}
