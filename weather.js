1acbbec83770d5611fd14507e351a07c42cd8129
stephanista <houser16@uw.edu>
2017-07-17 21:12:07 -0700

    Figured out the weather description issue and also found a handy way to uppercase the first letter

--- weather.js
+++ weather.js
@@ -2,7 +2,7 @@ const apiURL = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.or
 const appID = "a4192ac11c66b463d93c577e64fda186"; //yes thats mine
 let debug = null
 
-londonlink = "https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?lat=51.5074&lon=0.1278&appid=a4192ac11c66b463d93c577e64fda186"
+londonlink = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat=51.5074&lon=0.1278&appid=a4192ac11c66b463d93c577e64fda186"
 
 seattlelink = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat=47.6762&lon=-122.3182&appid=a4192ac11c66b463d93c577e64fda186"
 
@@ -22,11 +22,11 @@ function getSeaWx() {
     let response = JSON.parse(request.response)
     console.log(response.body)
     console.log(response.name)
-    console.log(response.weather)
+    console.log(response.weather[0].description)
     console.log(response.weather.main)
     console.log(response.main.temp)
     wxNameDiv.innerHTML = ("Station location: " + response.name)
-    wxGenDiv.innerHTML = (response.weather + "why is this not letting me grab weather object keys but the main and wind ones work? I want to see partly cloudyyy")
+    wxGenDiv.innerHTML = (response.weather[0].description.charAt(0).toUpperCase() + response.weather[0].description.slice(1))
     wxTempDiv.innerHTML = (response.main.temp + " degrees Kelvin (google is your friend to convert.. jk I'll make a conversion function)")
     wxHumDiv.innerHTML = (response.main.humidity + "% humidity")
     wxWindsDiv.innerHTML = ("Winds " + response.wind.speed + " at " + response.wind.deg)
@@ -40,5 +40,32 @@ function getSeaWx() {
 
 
 function getLondonWx() {
-
+  var request = new XMLHttpRequest()
+  console.log("request var set")
+  request.open("GET", londonlink, true)
+  console.log("request opened")
+  request.onload = function () {
+    console.log("hit onload")
+    let wxNameDiv = document.getElementById("displaywxname")
+    let wxGenDiv = document.getElementById("displaywxgen")
+    let wxTempDiv = document.getElementById("displaywxtemp")
+    let wxHumDiv = document.getElementById("displaywxhum")
+    let wxWindsDiv = document.getElementById("displaywxwinds")
+    let response = JSON.parse(request.response)
+    console.log(response.body)
+    console.log(response.name)
+    console.log(response.weather)
+    console.log(response.weather.main)
+    console.log(response.main.temp)
+    wxNameDiv.innerHTML = ("Station location: " + response.name)
+    wxGenDiv.innerHTML = (response.weather + "why is this not letting me grab weather object keys but the main and wind ones work? I want to see clouds")
+    wxTempDiv.innerHTML = (response.main.temp + " degrees Kelvin (google is your friend to convert.. jk I'll make a conversion function)")
+    wxHumDiv.innerHTML = (response.main.humidity + "% humidity")
+    wxWindsDiv.innerHTML = ("Winds " + response.wind.speed + " at " + response.wind.deg)
+  }
+  request.error = function (errorObject) {
+    console.log("Oops.. You bwoke it.")
+    console.log(errorObject)
+  }
+  request.send()
   }
